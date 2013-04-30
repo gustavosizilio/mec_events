3dc8a921bce2759e37a4c6715836380d68ee6609
Gustavo Sizilio <gustavo@buda.(none)>
2013-04-30 18:56:11 -0300

    reconfiguring issue type to tracker field

--- app/models/event_configuration.rb
+++ app/models/event_configuration.rb
@@ -1,11 +1,11 @@
 class EventConfiguration < ActiveRecord::Base
   include Redmine::SafeAttributes  
-  belongs_to :event_issue_category, :class_name => IssueCategory
-  belongs_to :participation_issue_category, :class_name => IssueCategory
+  belongs_to :event_tracker, :class_name => Tracker
+  belongs_to :participation_tracker, :class_name => Tracker
   belongs_to :project
 
-  safe_attributes 'event_issue_category_id',
-    'participation_issue_category_id',
+  safe_attributes 'event_tracker_id',
+    'participation_tracker_id',
     'project_id'
 
 end
--- db/migrate/001_create_event_configurations.rb
+++ db/migrate/001_create_event_configurations.rb
@@ -1,8 +1,8 @@
 class CreateEventConfigurations < ActiveRecord::Migration
   def change
     create_table :event_configurations do |t|
-      t.integer :event_issue_category_id
-      t.integer :participation_issue_category_id
+      t.integer :event_tracker_id
+      t.integer :participation_tracker_id
       t.integer :project_id, :references => :project
     end
   end
