8e897c4ae594fd57b060eb4abba373f56475b660
Gustavo Sizilio <gustavo@buda.(none)>
2013-05-01 18:13:53 -0300

    wip - implement send invitations

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,6 +3,32 @@ class EventsController < ApplicationController
 
   # Send invitations for users
   def send_invitations
+    
+    @issues = []
+    @errors = []
+    @event_configuration = EventConfiguration.find_by_issue(@issue)
+    EventConfiguration.get_participants(@issue).each do |user|
+      principal = Principal.find(user.id)
+      issue = Issue.where(:parent_id => @issue, :assigned_to_id => principal, :tracker_id => @event_configuration.participation_tracker).first
+      issue ||= Issue.new
+      
+      if issue.new_record? 
+        issue.parent_issue_id = @issue.id
+        issue.author = @issue.author
+        issue.assigned_to_id = principal.id
+
+        issue.subject = "Teste"
+        issue.project = @issue.project
+        issue.tracker = @event_configuration.participation_tracker
+      end
+      if issue.save
+        #TO-DO...
+      else
+        #TO-DO catch errors
+        @errors << issue.errors.full_messages
+      end
+    end
+
     redirect_to issue_path(@issue)
   end
 end
--- app/models/event_configuration.rb
+++ app/models/event_configuration.rb
@@ -10,9 +10,32 @@ class EventConfiguration < ActiveRecord::Base
     'project_id',
     'custom_field_participants_id'
 
+  def self.find_by_issue issue
+    EventConfiguration.where(:project_id => issue.project.id).last
+  end
+
   def self.tracker? issue
     @event_configuration = EventConfiguration.where(:project_id => issue.project.id).last
     return false if @event_configuration.nil?
     return @event_configuration.event_tracker_id == issue.tracker.id;
+  end
+
+  def self.get_participants issue
+    @participants = []
+    @event_configuration = EventConfiguration.where(:project_id => issue.project.id).last
+    return @participants if @event_configuration.nil?
+    
+    if(issue.available_custom_fields.include?(@event_configuration.custom_field_participants))
+      @custom_field_participants_value = issue.custom_field_values.select { |cv|
+        cv.custom_field_id == @event_configuration.custom_field_participants_id
+      }[0]
+
+      unless(@custom_field_participants_value.nil?)
+        @custom_field_participants_value.value.each do |participant_id|
+          @participants << User.find(participant_id) 
+        end 
+      end
+    end
+    @participants
   end
 end
